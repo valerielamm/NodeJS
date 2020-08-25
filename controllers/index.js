3598fc77bbccc14ad6aa5e7d22ea2cdda3c62a18
Valerie <valerielamm.com@gmail.com>
2020-08-25 14:46:05 -0500

    Let example.pug accept random parameter
    The website reads it back to you...

--- controllers/index.js
+++ controllers/index.js
@@ -7,3 +7,8 @@ exports.index = function(req, res, next) {
 exports.example = function(req, res, next) {
   res.render('example', { title: 'Example Page'});
 }
+
+exports.example_rand_number = function(req, res, next) {
+  var rand_num = req.params.rand_number;
+  res.render('example', { title: 'Example Page With Random Number Parameter', rand_number: rand_num});
+}
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ let index = require('../controllers/index')
 
 /* GET home page. */
 router.get('/', index.index);
-
 router.get('/example', index.example);
+router.get('/example/:rand_number', index.example_rand_number);
 
 module.exports = router;
